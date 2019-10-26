a57d0190990e60e89e0c763d773d4c90cd55ff8d
harbar20 <30803341+harbar20@users.noreply.github.com>
2019-10-26 14:11:11 -0400

    test
    123

---
+++ www/js/index.js
@@ -0,0 +1,7 @@
+
+/*
+this array is for the life expectancy calculator for your dog.
+WARNING. THIS IS A GIMMICK. DO NOT TAKE THIS NUMBER SERIOUSLY. 
+This info is based on this study: 
+https:www.psychologytoday.com/us/blog/canine-corner/201505/how-long-will-your-dog-live
+*/
